a7d87875c3199cd8d05cf4d14c7e567e1fb8e13a
MichalRais <Michal.Rais@iCloud.com>
2025-09-08 11:42:19 +0100

    Fixed

--- src/componenets/CounterList.js
+++ src/componenets/CounterList.js
@@ -13,24 +13,23 @@ const CounterList = () => {
 
     const handleRemove = (id) => {
         setCounters(counters.filter(a => a !== id))
+        setResults((prev) => prev.filter(r => r.id !== id))
     }
 
     const handleResults = (id, x) => {
         let oneResult = {id, x}
-        if(results = []) {
-           setResults([oneResult]) 
-        } else {
-            setResults(prev => [...prev, oneResult])
-        }
-        console.log(results);        
+        setResults((prev => {
+            const filtered = prev.filter((r) => r.id !== id)
+            return [...filtered, oneResult]
+        }))        
     }
 
-    // fix xxxxxxxxxxx
+    const total = results.reduce((acc, r) => acc + r.x, 0)
 
 
   return <div className="cl-container">
     <div className="counter-top">
-    <p>Total Sum: 00</p>
+    <p>Total Sum: {total}</p>
     <button onClick={handleAdd}>Add</button>
     <button>Reset</button>
     </div>
