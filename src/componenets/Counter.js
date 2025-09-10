5d27ee1d70c4888434f5ea5906989dc5e6573c52
MichalRais <Michal.Rais@iCloud.com>
2025-09-10 08:31:53 +0100

    feat(Counter): add h3 and buttons for plus, minus and remove

--- src/componenets/Counter.js
+++ src/componenets/Counter.js
@@ -1,9 +1,14 @@
 import "./Counter.css"
 
 const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+  return <div>
+    <h3>Id:X</h3>
+    <h3>Result:{}</h3>
+    <button>Plus</button>
+    <button>Minus</button>
+    <button>Remove</button>
+  </div>
+  
 }
 
 export default Counter
