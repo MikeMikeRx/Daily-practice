8f188016b514c9cd7ba22bbabb1156ae08395693
MichalRais <Michal.Rais@iCloud.com>
2025-09-10 09:22:18 +0100

    feat(CounterList): create counters state

--- src/componenets/CounterList.js
+++ src/componenets/CounterList.js
@@ -3,6 +3,8 @@ import "./CounterList.css"
 import { useState } from "react"
 
 const CounterList = () => {
+    const[counters, setCounters] = useState([1,2,3])
+
   return <div className="counter-list">
     <Counter/>
     </div>
