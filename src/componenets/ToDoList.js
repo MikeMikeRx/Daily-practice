f8d5d87033947001d1a9598b5170c3b04b37846b
MichalRais <Michal.Rais@iCloud.com>
2025-09-11 08:42:01 +0100

    feat(ToDoList.js): Add divs with classes to separate sections

--- src/componenets/ToDoList.js
+++ src/componenets/ToDoList.js
@@ -1,9 +1,13 @@
 import OneTask from "./OneTask"
 
 const ToDoList = () => {
-  return (
-    <div><OneTask/></div>
-  )
+  return <div className="container">
+    <div className="add-form">
+    </div> 
+    <div className="toDo-list">
+        <OneTask/></div>
+  </div>
+  
 }
 
 export default ToDoList
