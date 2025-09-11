e13396da3bcb936c1d46cfc17a3608546edfc3e6
MichalRais <Michal.Rais@iCloud.com>
2025-09-11 08:44:03 +0100

    feat(ToDoList.js): Add form with inputs type text and submit

--- src/componenets/ToDoList.js
+++ src/componenets/ToDoList.js
@@ -3,6 +3,10 @@ import OneTask from "./OneTask"
 const ToDoList = () => {
   return <div className="container">
     <div className="add-form">
+        <form>
+            <input type="text" />
+            <input type="submit" />
+        </form>        
     </div> 
     <div className="toDo-list">
         <OneTask/></div>
