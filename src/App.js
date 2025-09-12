157a1a7eafc96ad173175ad2f8179efbd0812c7b
MichalRais <Michal.Rais@iCloud.com>
2025-09-12 19:03:18 +0100

    feat: create ToDo state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
+  const[ToDo, setToDo] = useState({text:""})
+  
   return (
     <div>
       <input type="text" />
