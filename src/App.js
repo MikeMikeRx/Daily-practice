c12c9dea458cb0468b9f12ca6602cf43d6d97e15
MichalRais <Michal.Rais@iCloud.com>
2025-09-13 19:07:38 +0100

    Initialize new daily project to-do-app*

--- src/App.js
+++ src/App.js
@@ -1,62 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
 
 const App = () => {
-  const[toDo, setToDo] = useState("")
-  const[allToDos, setAllToDos] = useState([])
-
-  
-  const handleChange = (e) => {
-    // e.preventDefault()
-    const task = e.target.value
-    setToDo(task)
-    // console.log(toDo);    
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const newToDo = {id: new Date().getTime(),text: toDo, done: false}    
-    setAllToDos( (allToDos) => {
-      return [...allToDos, newToDo]
-    })
-    console.log(newToDo)  
-    // console.log(allToDos);  
-  }
-
-  const handleDone = (id) => {
-    setAllToDos(prev =>
-      prev.map(toDo =>
-        toDo.id === id ? { ...toDo, done: !toDo.done } : toDo
-      )
-    )
-  }
-
-  const handleDelete = (id) => {
-    setAllToDos( allToDos.filter(a => a.id !== id))
-  }
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} value={toDo.text}/>
-        <input type="submit"/>
-      </form>
-
-      {allToDos.map(item => (
-        <OneToDo key={item.id} id={item.id} text={item.text} handleDelete={handleDelete} handleDone={handleDone} done={item.done}/>
-      ))}
-    </div>
+    <div>App</div>
   )
 }
-
-const OneToDo = ({ id, text , done, handleDelete, handleDone }) => {
-
-  return <>
-  <h3>{text}</h3>
-  <input type="checkbox" checked={done} onChange={()=>handleDone(id)}/> 
-  <button onClick={()=>{handleDelete(id)}}>Delete</button>
-  </>
-}
-
 
 export default App
