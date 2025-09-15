88fb7885aca2c35e69fc1b76aee62563daaab673
MichalRais <Michal.Rais@iCloud.com>
2025-09-15 08:16:40 +0100

    initialize new daily project to-do-app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
-import { use, useState } from "react"
+import React from 'react'
 
 const App = () => {
-  const[toDo, setToDo] = useState({text:""})
-  const[allToDos, setAllToDos] = useState([])
-
-  const handleChange = (e) => {
-    setToDo({text: e.target.value}) 
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setAllToDos([...allToDos,{...toDo, id: new Date().getTime()}])    
-  }
-
-  const handleRemove = (id) => {
-    setAllToDos((allToDos) => {
-      return (allToDos.filter(a => a.id !== id) )
-    })
-  }
-
-  return <div>
-    <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleChange} value={toDo.text}/>
-      <input type="submit" />
-    </form>
-
-    {allToDos.map((item => (
-      <ToDo key={item.id} id={item.id} text={item.text} handleRemove={handleRemove}/>
-    )))}
-
-  </div>
-  
-}
-
-const ToDo = ({ id, text, handleRemove }) => {
-  return <div>
-    <h3>{text}</h3>
-    <input type="checkbox" />
-    <button onClick={()=>{handleRemove(id)}}>Remove</button>
-  </div>
+  return (
+    <div>App</div>
+  )
 }
 
 export default App
