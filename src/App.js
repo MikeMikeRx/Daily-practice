cf1e9c49a467ff3a011b67eba66b4fbc2459452a
MichalRais <Michal.Rais@iCloud.com>
2025-09-15 08:20:33 +0100

    feat(App): add form to div

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <div>
+    <form>
+
+    </form>
+  </div>
+  
 }
 
 export default App
