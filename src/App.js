8fe00196621d2558d00a29eeaf80d2c8c91cf914
MichalRais <Michal.Rais@iCloud.com>
2025-09-15 09:41:48 +0100

    feat(App): pass handleToggle function as props to ToDoList component

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
     </form>
 
     {allTasks.map((item) => (
-      <ToDoList key={item.id} id={item.id} text={item.text} done={item.done}/>
+      <ToDoList key={item.id} id={item.id} text={item.text} done={item.done} handleToggle={handleToggle}/>
     ))}
   </div>
   
