7447522419e751883f3786b26fcd0915a7c4524e
MichalRais <Michal.Rais@iCloud.com>
2025-09-16 08:54:33 +0100

    Initialize new daily project to-do-app*

--- src/App.js
+++ src/App.js
@@ -1,63 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
 
 const App = () => {
-  const[toDo, setToDo] = useState({text:""})
-  const[allTasks, setAllTasks] = useState([])
-
-  const handleChange = (e) => {
-    setToDo({text: e.target.value})   
-  }
-
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-
-    const newTask = {
-      text: toDo.text, 
-      id: new Date().getTime(),
-      done: false
-    }      
-    
-    setAllTasks([...allTasks, newTask])
-  }
-
-  const handleToggle = (id) =>{
-    const updatedDone = allTasks.map(item => item.id === id ? {...item, done: !item.done} : item)
-    setAllTasks(updatedDone)      
-  }
-
-  const handleRemove = (id) => {
-    setAllTasks(allTasks.filter(a => a.id !== id))
-  }
-
-  const total = allTasks.length
-  const unDone = allTasks.filter(item => !item.done).length
-  const done = allTasks.filter(item => item.done).length 
-
-
-  return <div>
-    <h1>Total:{total}</h1>
-    <h1>Tasks to do:{unDone}</h1>
-    <h1>Tasks done:{done}</h1>
-
-    <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleChange} value={toDo.text}/>
-      <input type="submit" />
-    </form>
-
-    {allTasks.map((item) => (
-      <ToDoList key={item.id} id={item.id} text={item.text} done={item.done} handleToggle={handleToggle} handleRemove={handleRemove}/>
-    ))}
-  </div>
-  
-}
-
-const ToDoList = ({ id, text, done, handleToggle, handleRemove }) => {
-  return <div>
-    <h2>{text}</h2>
-    <input type="checkbox" onChange={()=>handleToggle(id)} value={done}/>
-    <button onClick={()=>handleRemove(id)}>REMOVE</button>
-  </div>
+  return (
+    <div>App</div>
+  )
 }
 
 export default App
