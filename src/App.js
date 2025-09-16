f8fe64ba07b6a3bb64349cb94af9183e35055dd4
MichalRais <Michal.Rais@iCloud.com>
2025-09-16 08:58:22 +0100

    feat(App): create toDo state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
+  const[toDo, setTodo] = useState({text:""})
+
   return <div>
     <form action="">
       <input type="text" />
       <input type="submit" />
     </form>
-  </div>
-  
+  </div>  
 }
 
 export default App
