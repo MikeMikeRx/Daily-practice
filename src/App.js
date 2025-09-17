1df890dea9322780d5ae29488193af1888aa3e03
MichalRais <Michal.Rais@iCloud.com>
2025-09-17 10:19:25 +0100

    refactor(App): create useEffect to save allExpenses to local storage

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ const App = () => {
     setAllExpenses([])
   }
 
-
+  useEffect(()=>{
+    localStorage.setItem("Expenses", (JSON.stringify(allExpenses)))
+  },[allExpenses])
 
   const results = allExpenses.map(item => {
     return item.amount
