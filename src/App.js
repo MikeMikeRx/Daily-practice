240370baaf87349991ca3ab918117bc55a48ffb5
MichalRais <Michal.Rais@iCloud.com>
2025-09-17 08:31:33 +0100

    feat(App): add onchage to setExpense on input type text and number

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const[expense, setExpense] = useState({Description:"", Amount:""})
+  const[expense, setExpense] = useState({description:"", amount:""})
   const[allExpenses, setAllExpenses] = useState([])
 
   return <div>
     <form>
       <label htmlFor="text">Description: </label>
-      <input type="text" id="text"/>
+      <input type="text" id="text" onChange={(e)=>setExpense({...expense, description: e.target.value})}/>
       <label htmlFor="number">Amount: </label>
-      <input type="number" id="number"/>
+      <input type="number" id="number" onChange={(e)=>setExpense({...expense, amount: e.target.value})}/>
       <input type="submit" />
     </form>
   </div>  
