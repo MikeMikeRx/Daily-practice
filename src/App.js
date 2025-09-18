67bad6c2296d2a6abba726ac7d06a92742f3ade5
MichalRais <Michal.Rais@iCloud.com>
2025-09-18 08:28:35 +0100

    feat(App): create state for contact list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 const App = () => {
   const[contact, setContact] = useState({name:"", surname:"", phone:""})
+  const[contactList, setContactList] = useState([])
 
   return (
     <form>
