e5002928618614c57019b29be78d28edc8507cf8
MichalRais <Michal.Rais@iCloud.com>
2025-09-18 08:22:58 +0100

    feat(App): add label for phone input

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
       <input type="text" id="name"/>
       <label htmlFor="surname">Surname: </label>
       <input type="text" id="surname"/>
+      <label htmlFor="phone">Phone number: </label>
       <input type="number" id="phone"/>
     </form>
   )
