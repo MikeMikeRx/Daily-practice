3aaf114052055fb36dfe8f787576fd5b9a33cfbd
MichalRais <Michal.Rais@iCloud.com>
2025-09-18 08:52:12 +0100

    feat(App): add onChange handler to each input

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ const App = () => {
   return (
     <form>
       <label htmlFor="name">Name: </label>
-      <input type="text" id="name" name="name" value={contact.name}/>
+      <input type="text" id="name" name="name" value={contact.name} onChange={handleChage}/>
       <label htmlFor="surname">Surname: </label>
-      <input type="text" id="surname" name="surname" value={contact.surname}/>
+      <input type="text" id="surname" name="surname" value={contact.surname} onChange={handleChage}/>
       <label htmlFor="phone">Phone number: </label>
-      <input type="number" id="phone" name="phone" value={contact.phone}/>
+      <input type="number" id="phone" name="phone" value={contact.phone} onChange={handleChage}/>
       <input type="submit" value="Add Contact"/>
     </form>
   )
