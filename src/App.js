2b60fec3f5da11b629a1eba36cf1e4f43c756c7d
MichalRais <Michal.Rais@iCloud.com>
2025-09-24 16:38:44 +0100

    feat: add label for input id surname

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
       <form>
         <label htmlFor="name">Name: </label>
         <input type="text" id="name"/>
+        <label htmlFor="surname">Surname: </label>
         <input type="text" id="surname"/>
       </form>
     </div>
