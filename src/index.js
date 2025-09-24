05fb3364c48d4376e6533356af936c0169441f5f
MichalRais <Michal.Rais@iCloud.com>
2025-09-24 16:30:51 +0100

    feat: active strict mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-
 import './index.css';
 import App from './App';
 
 
 const container = document.getElementById('result')
 const root = createRoot(container)
-root.render(<App tab="home" />)
+root.render(
+    <React.StrictMode>
+        <App tab="home" />
+    </React.StrictMode>
+)
