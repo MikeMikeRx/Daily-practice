39a0335229287278e364f97a84b5e995169df1df
MichalRais <Michal.Rais@iCloud.com>
2025-09-24 16:34:33 +0100

    feat: add const contactList state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState } from "react"
 
 const App = () => {
   const[contact, setContact] = useState({name:"", surname:"", phone:""})
+  const[contactList, setContactList] = useState([])
 
-  
+
   return (
     <div>App</div>
   )
