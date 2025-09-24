9401f7aa5fdf594cd75c6f2728133fd7ad4839a2
MichalRais <Michal.Rais@iCloud.com>
2025-09-24 16:39:50 +0100

    feat: add input type number with id phone

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
         <input type="text" id="name"/>
         <label htmlFor="surname">Surname: </label>
         <input type="text" id="surname"/>
+        <input type="number" id="phone"/>
       </form>
     </div>
   )
