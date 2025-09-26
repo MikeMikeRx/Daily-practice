4802c05757880d70db138841676207d1ea7d02ad
MichalRais <Michal.Rais@iCloud.com>
2025-09-26 18:28:56 +0100

    feat: add submit handler onSubmit to the form

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
         <input 
           type="text" 
