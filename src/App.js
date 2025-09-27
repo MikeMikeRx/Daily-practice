c91034a2f2c2dd5e559db9e60b387de705bad3c8
MichalRais <Michal.Rais@iCloud.com>
2025-09-27 17:37:51 +0100

    fix: Edit handler on clik on Edit button

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ const App = () => {
             <ul key={contact.id}>
               <p>name: <strong>{contact.name} {contact.surname}</strong></p>
               <p>phone: <strong>{contact.phone}</strong></p>
-              <button onClick={()=>setEditingId(contact.id)}>Edit</button>
+              <button onClick={()=>handleEdit(contact.id)}>Edit</button>
             </ul>
           ))}
         </li>
