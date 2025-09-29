6ebb647a3f4c02baa23772b23a36ad3aef4ff5e8
MichalRais <Michal.Rais@iCloud.com>
2025-09-29 08:06:22 +0100

    chore: Import useEffect from react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const App = () => {
   const [contact, setContact] = useState({firstName:"", lastName:"", phone:"", email:""})
