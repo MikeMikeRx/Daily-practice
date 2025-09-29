5443a73b1f0f63fe367100c82ab276f742c7fd22
MichalRais <Michal.Rais@iCloud.com>
2025-09-29 11:23:16 +0100

    feat(App): Pass down contact list as a props to ContactForm component

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div>
       <section className="AddContact-sec">
-        <ContactForm handleAddNew={handleAddNew}/>
+        <ContactForm handleAddNew={handleAddNew} contactList={contactList}/>
       </section>
 
       <section className="Search-sec">
