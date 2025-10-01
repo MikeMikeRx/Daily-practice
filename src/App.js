6f9773638a6fe914a0aeaf41782ed0035b704e94
MichalRais <Michal.Rais@iCloud.com>
2025-10-01 07:45:05 +0100

    feat(NoteForm): Attach change handler to note form inputs

--- src/App.js
+++ src/App.js
@@ -23,15 +23,15 @@ const NoteForm = () => {
   const handleChange = (e) =>{
     const name = e.target.name
     const value = e.target.value
-    setOneNote({...oneNote, [name]:value})
+    setOneNote({...oneNote, [name]:value})    
   }
 
   return (
     <form>
       <label htmlFor="title">Title: </label>
-      <input type="text" id="title" name="title" value={oneNote.title}/>
+      <input type="text" id="title" name="title" value={oneNote.title} onChange={handleChange}/>
       <label htmlFor="body">Body: </label>
-      <input type="text" id="body" name="body" value={oneNote.body}/>
+      <input type="text" id="body" name="body" value={oneNote.body} onChange={handleChange}/>
       <button type="submit"> Add Note</button>
     </form>
   )
