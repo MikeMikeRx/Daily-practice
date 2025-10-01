fba8c611370f64a5190fe9506dbf6a73e6cdc95f
MichalRais <Michal.Rais@iCloud.com>
2025-10-01 07:21:54 +0100

    feat(App): Notes state is loaded from local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react"
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  
+  const [notes, setNotes] = useState(()=>{
+    const loadLs = JSON.parse(localStorage.getItem("notes"))
+    return loadLs ? loadLs : []  
+  })
+
   return (
     <div>App</div>
   )
