d9b5b5f406b1a516e51334edce27fd7a69f3bc6b
MichalRais <Michal.Rais@iCloud.com>
2025-10-04 17:32:30 +0100

    feat(App): Initialize notes state using localStorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react"
 
 const App = () => {
+  const [notes, setNotes] = useState(()=>{
+    const loadLS = JSON.parse(localStorage.getItem("Notes"))
+    return loadLS ? loadLS : []
+  })
+
   return (
     <div>App</div>
   )
