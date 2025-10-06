b41c3fd7101fdaf15569481cbf78aee34d165410
MichalRais <Michal.Rais@iCloud.com>
2025-10-06 07:17:56 +0100

    feat(App): Add filter state for task list view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 
 const App = () => {
   const [tasks, setTask] = useState([])
+  const [filter, setFilter] = ("all", "active", "completed")
 
   return (
     <div>App</div>
